ade86d310bfe913d12650876b2840c23d148c73f
wen.dong <wen.dong@lmitechnologies.com>
2022-11-15 11:38:31 -0500

    add meaningful default values for pipeline configs

--- pipeline/pipeline_class.py
+++ pipeline/pipeline_class.py
@@ -29,7 +29,10 @@ class ModelPipeline:
         """
         init the pipeline with kwargs
         """
-        pass
+        self.configs = {}
+        self.configs['engine_path'] = os.path.realpath(os.path.expandvars(kwargs.get('engine_file',"")))
+        self.configs['class_map'] = {int(k):v for k,v in kwargs.get('class_map', {}).items()}
+        pass # add your code here ...
         
         
     def load(self):
